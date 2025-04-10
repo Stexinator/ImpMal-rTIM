e0cba734bd31f6f6fa38e0f78e3ab2535340b291
Havoclivekiller <havoclivekiller@gmail.com>
2025-04-10 11:38:42 +0200

    added alternative initiative formula

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,2 +1,31 @@
-Hooks.once('init', () => {
+Hooks.on('init', () => {
+    registerSettings();
+
+    if (game.settings.get("impmal-rtim", "alternativeInitiative") == true) {
+        CONFIG.Combat.initiative = {
+            formula: game.settings.get("impmal-rtim", "alternativeInitiativeFormula"),
+        };
+    }
 });
+
+
+function registerSettings() {
+    game.settings.register("impmal-rtim", "alternativeInitiative", {
+        name: "Alternative Initiative",
+        hint: "Use alternative initiative from the formula below. Requires restart.",
+        scope: "world",
+        config: true,
+        default: false,
+        requiresReload: true,
+        type: Boolean
+    });
+
+    game.settings.register("impmal-rtim", "alternativeInitiativeFormula", {
+        name: "Alternative Initiative Formula",
+        scope: "world",
+        config: true,
+        default: "d10 + @combat.initiative + @skills.reflexes.total/100",
+        requiresReload: true,
+        type: String
+    });
+}
